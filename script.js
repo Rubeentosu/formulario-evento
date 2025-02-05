5841243c4db6348e8cb61b4e3fb9f34fd188023a
Rafa <rafajr90@hotmail.com>
2025-02-05 15:50:20 +0100

    Actualizacion

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function validarUsuario(){
+function validarUsuario(usuario){
+
+}
+
+function validarApellidos(){
     
+}
+
+function validarEdad(edad){
+
+}
+
+function validarEmail(mail){
+    const rege = /^[a-zA-Z0-9_.-]+@[a-z0-9_-]+(\.[a-z]{2,4}){1,2}$/ ;
+    return rege.test(mail);
 }
